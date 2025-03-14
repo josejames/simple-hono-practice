b7fd4aa0347b61c3a49485e212149f559828858b
Wong199 <alex.jimenez.wong@hotmail.com>
2025-03-13 19:00:52 -0600

    añadir tabla de futbolistas

--- src/index.ts
+++ src/index.ts
@@ -9,15 +9,15 @@ const app = new Hono()
 app.use('*', cors())
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('Hello Wong!')
 })
 
 const sequelize = new Sequelize({
-    dialect: 'mysql',
-    host: 'localhost',
-    username: 'root',
+    dialect: '',
+    host: '',
+    username: '',
     password: '',
-    database: 'hono'
+    database: ''
 }); // Example for SQLite, change as needed
 
 const User = sequelize.define('User', {
@@ -32,18 +32,46 @@ const User = sequelize.define('User', {
     }
 });
 
-app.post('/api/users', async (c) => {
+const futbolistas = sequelize.define('Futbolista' ,{
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    posicion: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    numero: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+})
+
+/*app.post('/api/users', async (c) => {
     const { name, email } = await c.req.json();
     try {
         await User.sync();
+        await futbolistas.sync();
         const user = await User.create({ name, email });
         return c.json({ message: 'User inserted successfully!', user });
     } catch (error) {
         const errorMessage = (error as Error).message;
         return c.json({ message: 'Failed to insert user.', error: errorMessage }, 500);
     }
+});*/
+app.post('/api/futbolistas', async (c) => {
+    console.log("error")
+    const { name, Posicion , Numero } = await c.req.json();
+    try {
+        await futbolistas.sync();
+        const user = await futbolistas.create({ name: name, posicion:Posicion,numero:Numero });
+        return c.json({ message: 'User inserted successfully!', user });
+    } catch (error) {
+        console.log(error)
+        const errorMessage = (error as Error).message;
+        return c.json({ message: 'Failed to insert user.', error: errorMessage }, 500);
+    }
 });
-
 // connect to the database
 sequelize.authenticate()
     .then(() => {
